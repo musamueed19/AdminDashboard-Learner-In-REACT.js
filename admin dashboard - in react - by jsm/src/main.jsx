1ec720d97998bb06c05a11301b119a20ec2146f2
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-10-26 16:23:15 +0500

    Project Setup - Issue - Tailwindcss is not working

--- admin dashboard - in react - by jsm/src/main.jsx
+++ admin dashboard - in react - by jsm/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
+
+{
+  /*import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import './index.css'
-import App from './App.jsx'
+
+import App from "./App";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
+*/
+}
